1d041009747d1ad86e3101a39509ea63d23fb859
Ho-Yeon Kim <mr.foundation21@gmail.com>
2014-01-03 02:10:07 +0900

    ADD AI DLL Library

---
+++ AI/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AI
+{
+    class State
+    {
+        public State()
+        {
+        }
+        enum states
+        {
+            attack,
+            defence,
+
+            freethrow,
+            jumpball,
+            outofbound,
+            strategytime
+        }
+    }
+}
