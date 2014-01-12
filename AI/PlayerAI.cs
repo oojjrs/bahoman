3290fbfddddf0e86d145441c0d626d3ba7ccfc37
Ho-Yeon Kim <mr.foundation21@gmail.com>
2014-01-13 00:57:26 +0900

    PlayerAI Rebound add

--- AI/PlayerAI.cs
+++ AI/PlayerAI.cs
@@ -6,6 +6,7 @@ namespace AI
 {
     class PlayerAI
     {
+        private Point ringpoint = new Point(50, 50);
         //private Boolean hasBall = false;
         public PlayerState GetCurrentPlayerState()
         {
@@ -29,15 +30,12 @@ namespace AI
             }
         }
 
-        public PlayerState Determine(State currentstate)
+        public PlayerState Determine(State currentstate, Point playerposition)
         {
             if (GetCurrentPlayerState() == PlayerState.Dribble)
             {
-                Point playerpoint = new Point(0, 0);
-                Point ringpoint = new Point(50, 50);
-
                 //슛이 가능한지 현재 위치 확인
-                if (GetShootingPoint(playerpoint, ringpoint) > 80)
+                if (GetShootingPoint(playerposition, ringpoint) > 80)
                 {
                     //현재 상태를 슛상태로 변환
                     return PlayerState.Shoot;
@@ -52,7 +50,16 @@ namespace AI
             {
                 //Shooting()
                 //슛상태일때는 슛하고 리바운드 혹은 수비 준비 해야지;
-                return PlayerState.Rebound;
+                float distancefromRing = PhysicsEngine.GetDistance(playerposition, ringpoint);
+                if (distancefromRing < 10)
+                {
+                    return PlayerState.Rebound;
+                }
+                else
+                {
+                    return PlayerState.Free;
+                }
+                
             }
             else
             {
