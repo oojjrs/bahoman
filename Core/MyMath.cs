e4b02f682dba11ee218a35d584e7e0844e942197
oojjrs <oojjrs@hanmail.net>
2014-01-17 01:18:34 +0900

    Warning as Error, Add MyMath

---
+++ Core/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public static class MyMath
+    {
+        public static double DegreeToRadian(double degree)
+        {
+            return Math.PI / 180.0 * degree;
+        }
+
+        public static double RadianToDegree(double radian)
+        {
+            return 180.0 / Math.PI * radian;
+        }
+    }
+}
