61f2a6c18a15aa9c6ef869f495bc45a5577bb6b7
oojjrs <oojjrs@hanmail.net>
2014-01-30 01:58:02 +0900

    Unique ID 구조체 추가

---
+++ bball/UID.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bball
+{
+    struct UID
+    {
+        public enum Type
+        {
+            Player,
+            Team,
+        }
+
+        public static readonly UID Unknown = new UID(0);
+
+        private static int seqPlayer = 0;
+        private static int seqTeam = 0;
+
+        private readonly int value;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (this.value == UID.Unknown.value)
+                return false;
+            return this.value == ((UID)obj).value;
+        }
+
+        public override int GetHashCode()
+        {
+            return value;
+        }
+
+        public static bool operator ==(UID l, UID r)
+        {
+            return l.Equals(r);
+        }
+
+        public static bool operator !=(UID l, UID r)
+        {
+            return !l.Equals(r);
+        }
+
+        private UID(int v)
+        {
+            value = v;
+        }
+
+        public static UID Create(Type t)
+        {
+            switch(t)
+            {
+                case Type.Player:
+                    return new UID(++seqPlayer);
+                case Type.Team:
+                    return new UID(0x10000000 + ++seqTeam);
+                default:
+                    throw new Exception("UID 새 타입에 대한 핸들링이 필요합니다");
+            }
+        }
+    }
+}
