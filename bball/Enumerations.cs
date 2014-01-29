a053f111589634a5994337e15e6a8e1b31d7521d
oojjrs <oojjrs@hanmail.net>
2014-01-29 19:37:06 +0900

    포지션 추가

---
+++ bball/Enumerations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bball
+{
+    enum Position
+    {
+        Bench,
+        PointGuard,
+        ShootingGuard,
+        SmallFoward,
+        PowerFoward,
+        Center,
+    }
+}
