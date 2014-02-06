de2f8af58e28ba128d6c6bea62c071a784b49c6e
Ho-Yeon Kim <mr.foundation21@gmail.com>
2014-02-07 01:09:52 +0900

    AwarenessInfo add

--- AI/AwarenessInfo.cs
+++ AI/AwarenessInfo.cs
@@ -11,41 +11,48 @@ namespace AI
         private CourtPos playerLocation;
         private PlayerState playerState;
         private Boolean isTeammate;
+        private int backNumber;
         private Boolean hasBall;
-        private int lastAwarenessTick = 0;
+        private int lastAwarenessTick;
 
-        public PlayerAwareness(CourtPos location, PlayerState state, Boolean isteammate, Boolean hasball, int lastawarenesstick)
+        public PlayerAwareness(CourtPos location, PlayerState state, Boolean isteammate, int backnumber ,Boolean hasball, int lastawarenesstick)
         {
             playerLocation = location;
             playerState = state;
             isTeammate = isteammate;
             hasBall = hasball;
+            backNumber = backnumber;
             lastAwarenessTick = lastawarenesstick;
         }
 
         public PlayerState PlayerState
         {
             get { return playerState; }
+            set { playerState = value; }
         }
 
         public CourtPos Location
         {
             get { return playerLocation; }
+            set { playerLocation = value; }
         }
 
         public Boolean HasBall
         {
             get { return hasBall; }
+            set { hasBall = value; }
         }
 
         public Boolean IsTeammate
         {
             get { return isTeammate; }
+            set { isTeammate = value; }
         }
 
         public int LastAwarenessTick
         {
             get { return lastAwarenessTick; }
+            set { lastAwarenessTick = value; }
         }
     }
 
@@ -65,21 +72,41 @@ namespace AI
         public BallState BallState
         {
             get { return ballState; }
+            set { ballState = value; }
         }
 
         public CourtPos Location
         {
             get { return ballLocation; }
+            set { ballLocation = value; }
         }
 
         public int LastAwarenessTick
         {
             get { return lastAwarenessTick; }
+            set { lastAwarenessTick = value; }
         }
     }
     
     class AwarenessInfo
     {
-        
+        private List<PlayerAwareness> playerAwarenessInfos = new List<PlayerAwareness>();
+        private BallAwareness ballInfo = null;
+
+        public AwarenessInfo()
+        { 
+        }
+
+        public List<PlayerAwareness> PlayerAwarenessInfos
+        {
+            get { return playerAwarenessInfos; }
+            set { playerAwarenessInfos = value; }
+        }
+
+        public BallAwareness BallInfo
+        {
+            get { return ballInfo; }
+            set { ballInfo = value; }
+        }
     }
 }
